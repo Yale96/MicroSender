0c15ac388b5300b91590b9284ed3b0493870e4b6
Yannick van Leeuwen <yannickvanleeuwen@i-lion.nl>
2017-11-15 14:35:56 +0100

    Repository updated

--- DemoSend/Repositorys/MessageRepository.cs
+++ DemoSend/Repositorys/MessageRepository.cs
@@ -59,63 +59,57 @@ namespace DemoSend.Repositorys
             {
                 new Message
                         {
-                            Id = 1,
-                            Application = "Werknemerloket",
-                            TimeStamp = "13:30",
-                            LogLevel = "Verbose",
-                            Location = "/Werknemerloket/login.aspx",
-                            Content = "Werknemer kan niet inloggen."
-
+                          Id = 1,
+                          Application = "Werknemerloket",
+                          TimeStamp = "13:30",
+                          LogLevel = "Verbose",
+                          Location = "/Werknemerloket/login.aspx",
+                          Content = "Werknemer kan niet inloggen."
                         },
                         new Message
                         {
-                            Id = 2,
-                            Application = "Werkgeverloket",
-                            TimeStamp = "09:45",
-                            LogLevel = "Debug",
-                            Location = "/Werkgeverloket/login.aspx",
-                            Content = "Werkgever kan niet inloggen."
-
+                           Id = 2,
+                           Application = "Werkgeverloket",
+                           TimeStamp = "09:45",
+                           LogLevel = "Debug",
+                           Location = "/Werkgeverloket/login.aspx",
+                           Content = "Werkgever kan niet inloggen."
                         },
                         new Message
                         {
-                            Id = 3,
-                            Application = "Werknemerloket",
-                            TimeStamp = "15:18",
-                            LogLevel = "Information",
-                            Location = "/Werknemerloket/login.aspx",
-                            Content = "Werknemer kan niet inloggen."
-
+                           Id = 3,
+                           Application = "Werknemerloket",
+                           TimeStamp = "15:18",
+                           LogLevel = "Information",
+                           Location = "/Werknemerloket/login.aspx",
+                           Content = "Werknemer kan niet inloggen."
                         },
                         new Message
                         {
-                            Id = 4,
-                            Application = "Werkgeverloket",
-                            TimeStamp = "10:45",
-                            LogLevel = "Warning",
-                            Location = "/Werkgeverloket/login.aspx",
-                            Content = "Werkgever kan niet inloggen."
-
+                           Id = 4,
+                           Application = "Werkgeverloket",
+                           TimeStamp = "10:45",
+                           LogLevel = "Warning",
+                           Location = "/Werkgeverloket/login.aspx",
+                           Content = "Werkgever kan niet inloggen."
                         },
                         new Message
                         {
-                            Id = 5,
-                            Application = "Werknemerloket",
-                            TimeStamp = "17:30",
-                            LogLevel = "Error",
-                            Location = "/Werknemerloket/login.aspx",
-                            Content = "Werknemer kan niet inloggen."
-
+                           Id = 5,
+                           Application = "Werknemerloket",
+                           TimeStamp = "17:30",
+                           LogLevel = "Error",
+                           Location = "/Werknemerloket/login.aspx",
+                           Content = "Werknemer kan niet inloggen."
                         },
                         new Message
                         {
-                            Id = 6,
-                            Application = "Werkgeverloket",
-                            TimeStamp = "19:35",
-                            LogLevel = "Fatal",
-                            Location = "/Werkgeverloket/login.aspx",
-                            Content = "Werkgever kan niet inloggen."
-
+                           Id = 6,
+                           Application = "Werkgeverloket",
+                           TimeStamp = "19:35",
+                           LogLevel = "Fatal",
+                           Location = "/Werkgeverloket/login.aspx",
+                           Content = "Werkgever kan niet inloggen."
                         }
             };
         }
